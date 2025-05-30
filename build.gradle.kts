929dee059566ac2bd7187ba36749dafaf33c5af6
onecripshama <onecripnigga@yandex.ru>
2025-05-30 23:53:00 +0300

    Initial commit 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,15 @@ repositories {
 
 dependencies {
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.7")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0") // Для улучшенных HTTP-запросов
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // Для асинхронности
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation(kotlin("test"))
 }
+
+application {
+    mainClass.set("bot.MainKt")
+}
+
 
 tasks.test {
     useJUnitPlatform()
